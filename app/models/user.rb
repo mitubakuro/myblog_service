b3c8121a686a69f1619056dc8be12c9cf5a8892c
mitubakuro <nakano1113mituba@gmail.com>
2020-09-08 19:06:31 +0900

    gem導入、環境変数の設定、アイコン設置、deviseのコントローラ呼び出し

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   has_many :articles
+  has_many :sns_credentials
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 end
