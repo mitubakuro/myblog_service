d738365bbd971fa14d50edb3f597ae3e36ffe1c3
mitubakuro <nakano1113mituba@gmail.com>
2020-09-08 19:36:07 +0900

    deviseのルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  # devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
+
   resources :articles do
     collection do
       get "search"
