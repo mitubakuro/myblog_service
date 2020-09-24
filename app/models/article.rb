b6f71471f0d34f201348c67932ae4640378bf8bf
mitubakuro <nakano1113mituba@gmail.com>
2020-09-24 11:50:04 +0900

    通知機能のコントローラ

--- app/models/article.rb
+++ app/models/article.rb
@@ -18,4 +18,22 @@ class Article < ApplicationRecord
       Article.all.order(created_at: :desc)
     end
   end
+
+  def create_notification_like!(current_user)
+    # すでに「いいね」されているか検索
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and post_id = ? and action = ? ", current_user.id, user_id, id, 'like'])
+    # いいねされていない場合のみ、通知レコードを作成
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        post_id: id,
+        visited_id: user_id,
+        action: 'like'
+      )
+      # 自分の投稿に対するいいねの場合は、通知済みとする
+      if notification.visitor_id == notification.visited_id
+        notification.checked = true
+      end
+      notification.save if notification.valid?
+    end
+  end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,6 @@
 class Notification < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
+
   belongs_to :article, optional: true
   belongs_to :comment, optional: true
 
