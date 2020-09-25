29e8af41487e8f8f18a9787f5ee9e7b728d54701
mitubakuro <nakano1113mituba@gmail.com>
2020-09-25 11:56:36 +0900

    エラーハンドリング、ビュー微調整

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :top]
+  before_action :authenticate_user!, except: [:index, :top]
   before_action :find_article, only: [:show, :edit, :update, :destroy, :release, :unrelease]
   before_action :stop_direct_url, only:[:show, :edit, :update, :destroy]
 
