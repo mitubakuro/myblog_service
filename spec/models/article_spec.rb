c653819818df4a71526b3e2e21f197e98ed42dde
mitubakuro <nakano1113mituba@gmail.com>
2020-10-01 23:43:12 +0900

    モデル単体テストコードを追記

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
+# binding.pry
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'articleの新規作成' do
+    before do
+      @article = FactoryBot.build(:article)
+    end
+
+    it 'title, bodyが存在すれば登録できること' do
+      expect(@article).to be_valid
+    end
+
+    it 'titleが空では登録できないこと' do
+      @article.title = ""
+      @article.valid?
+      expect(@article.errors.full_messages).to include("Titleを入力してください")
+    end
+
+    it 'bodyが空では登録できないこと' do
+      @article.body = ""
+      @article.valid?
+      expect(@article.errors.full_messages).to include("Bodyを入力してください")
+    end
+
+    it 'ユーザーが紐付いていない投稿は保存できないこと' do
+      @article.user = nil
+      @article.valid?
+      expect(@article.errors.full_messages).to include("Userを入力してください")
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,6 @@
 require 'rails_helper'
+# binding.pry
+
 
 RSpec.describe User, type: :model do
   describe 'userの新規作成' do
@@ -11,20 +13,45 @@ RSpec.describe User, type: :model do
     end
 
     it 'nicknameが空では登録できないこと' do
-      @user.nickname = nil
+      @user.nickname = ""
       @user.valid?
       expect(@user.errors.full_messages).to include('ユーザー名を入力してください')
     end
 
     it 'emailが空では登録できないこと' do
-      @user.email = nil
+      @user.email = ""
       @user.valid?
       expect(@user.errors.full_messages).to include('Eメールを入力してください')
     end
 
     it 'passwordが空では登録できないこと' do
+      @user.password = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("パスワードを入力してください", "パスワード（確認用）とパスワードの入力が一致しません")
     end
+
     it 'passwordが存在してもpassword_confirmationが空では登録できないこと' do
+      @user.password_confirmation = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("パスワード（確認用）とパスワードの入力が一致しません")
+    end
+
+    it 'firstnameが空では登録できないこと' do
+      @user.firstname = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Firstnameを入力してください")
+    end
+
+    it 'lastnameが空では登録できないこと' do
+      @user.lastname = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Lastnameを入力してください")
+    end
+
+    it 'birthdayが空では登録できないこと' do
+      @user.birthday = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birthdayを入力してください")
     end
   end
 end
