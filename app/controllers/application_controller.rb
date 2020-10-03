92277c1dbbbe3253424d842e02a9161a4c6a6b9a
mitubakuro <nakano1113mituba@gmail.com>
2020-10-03 19:49:20 +0900

    docker環境ができた
    rails serverも問題なく動作したのでmergeします

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth
+  # before_action :basic_auth
 
   private
 
@@ -8,9 +8,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :firstname, :lastname, :birthday])
   end
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
-    end
-  end
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+  #   end
+  # end
 end
